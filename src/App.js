0269e7c98bdda14e14fe8e940c4b195da104bb85
diegofj <2018007451@unfv.edu.pe>
2023-04-02 11:05:36 -0500

    Create header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {
   ChakraProvider,
 } from '@chakra-ui/react';
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <ChakraProvider>
+      <Header></Header>
     </ChakraProvider>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+export default function Header(props) {
+    return <header>
+        <Text as='h1' fontSize='4xl' fontWeight='bold'>Diego Figueroa Juarez</Text>
+    </header>
+}
