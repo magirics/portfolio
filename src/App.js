9069680566989b647db102322bd2f29e738e2e67
diegofj <2018007451@unfv.edu.pe>
2023-04-02 11:27:48 -0500

    Create presentation card in main content

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <ChakraProvider>
       <Header></Header>
+      <Main></Main>
     </ChakraProvider>
   );
 }
