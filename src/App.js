a5ed7383c9a69c23b516b77654575293335eba2e
diegofj <2018007451@unfv.edu.pe>
2023-04-02 12:06:37 -0500

    Create footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <ChakraProvider>
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
     </ChakraProvider>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import { Box, Link, Text } from "@chakra-ui/react";
+
+export default function Footer(props) {
+    return <footer>
+      <Text as='h2' fontSize='2xl' fontWeight='semibold'>Contact</Text>
+      <Box width='20rem' display='inline-block' >
+        <Text fontWeight='semibold'>Email:</Text>
+        <Link href='mailto:diego.undefined@gmail.com'>diego.undefined@gmail.com</Link>
+      </Box>
+      <Box display='inline-block'>
+        <Text fontWeight='semibold'>Github:</Text>
+        <Link href='https://github.com/magirics' isExternal>magirics</Link>
+      </Box>
+      <Box></Box>
+      <Box width='20rem' display='inline-block'>
+        <Text fontWeight='semibold'>Phone:</Text>
+        <Link href='+51-968-973-223'>+51-968-973-223</Link>
+      </Box>
+      <Box display='inline-block'>
+        <Text fontWeight='semibold'>LinkedIn:</Text>
+        <Link href='https://linkedin.com/in/diego-figueroa-6b0986270' isExternal>Diego Figueroa</Link>
+      </Box>
+    </footer>
+  }
