fefb50af917fcf3a9283727e467bb02191a2bde3
Diego Figueroa Juarez <diego.undefined@gmail.com>
2026-07-30 12:08:15 -0500

    Change cellphone number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer(props) {
       <Box></Box>
       <Box width='20rem' display='inline-block'>
         <Text fontWeight='semibold'>Phone:</Text>
-        <Link href='tel:+51-968-973-223'>+51-968-973-223</Link>
+        <Link href='tel:+51-968-973-223'>+51 975 932 697</Link>
       </Box>
       <Box display='inline-block'>
         <Text fontWeight='semibold'>LinkedIn:</Text>
